4f8c8efb228a4fd0830739e0128ee15dff3dc43d
lavsam-dev <lavsam@gmail.com>
2022-01-08 18:44:12 +0400

    Initial commit

---
+++ app/src/main/java/lavsam/gb/testingl2/model/SearchResponse.kt
@@ -0,0 +1,13 @@
+package lavsam.gb.testingl2.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("total_count")
+    @Expose
+    val totalCount: Int?,
+    @SerializedName("items")
+    @Expose
+    val searchResults: List<SearchResult>?
+)
