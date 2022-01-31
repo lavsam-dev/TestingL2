d7912e39e50123624ee122899a8e54bcf84c1c29
lavsam-dev <lavsam@gmail.com>
2022-01-31 22:07:14 +0400

    Lesson7 recyclerview test 1

---
+++ app/src/androidTest/java/lavsam/gb/testingl2/recycler/MainActivityRecyclerViewTest.kt
@@ -0,0 +1,80 @@
+package lavsam.gb.testingl2.recycler
+
+import android.view.View
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import lavsam.gb.testingl2.R
+import lavsam.gb.testingl2.view.MainActivity
+import lavsam.gb.testingl2.view.SearchResultAdapter
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityRecyclerViewTest {
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun activitySearch_ScrollTo() {
+        loadList()
+
+        Espresso.onView(withId(R.id.recyclerView))
+            .perform(
+                RecyclerViewActions.scrollTo<SearchResultAdapter.SearchResultViewHolder>(
+                    ViewMatchers.hasDescendant(ViewMatchers.withText("cloudflare/cobweb"))
+                )
+            )
+    }
+
+    @Test
+    fun activitySearch_PerformClickAtPosition() {
+        loadList()
+
+        Espresso.onView(withId(R.id.recyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<SearchResultAdapter.SearchResultViewHolder>(
+                30, ViewActions.click()
+            )
+        )
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+
+    private fun loadList() {
+        Espresso.onView(withId(R.id.searchEditText)).perform(ViewActions.click())
+        Espresso.onView(withId(R.id.searchEditText))
+            .perform(ViewActions.replaceText("COBOL"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.searchEditText)).perform(ViewActions.pressImeActionButton())
+    }
+
+    private fun tapOnItemWithId(id: Int) = object : ViewAction {
+        override fun getConstraints(): Matcher<View>? {
+            return null
+        }
+
+        override fun getDescription(): String {
+            return "Tap on view with defined id"
+        }
+
+        override fun perform(uiController: UiController?, view: View?) {
+            val v = view?.findViewById(id) as View
+            v.performClick()
+        }
+    }
+}
---
+++ app/src/test/java/lavsam/gb/testingl2/SharedTestData.kt
@@ -0,0 +1,11 @@
+package lavsam.gb.testingl2
+
+internal const val TEST_FAKE_NUMBER_30 = 30
+internal const val TEST_NUMBER_OF_RESULTS_ZERO = "Number of results: 0"
+internal const val TEST_NUMBER_OF_RESULTS_PLUS_1 = "Number of results: 1"
+internal const val TEST_NUMBER_OF_RESULTS_MINUS_1 = "Number of results: -1"
+internal const val DEFAULT_NUMBER_ZERO = 0
+internal const val TEST_NUMBER_OF_RESULTS_42 = "Number of results: 42"
+internal const val TEXT_OF_BUTTON_TO_DETAILS = "to details"
+internal const val TIMEOUT = 10000L
+internal const val TEXT_OF_TEXTVIEW_TOTAL_COUNT = "totalCountTextView"
